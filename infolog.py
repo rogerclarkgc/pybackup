64c88f03f4b486a774e7f9159ec85b88ec9e00d2
rogerclarkgc <rogerclark@163.com>
2018-04-26 19:28:47 +0800

    ADD[roger]: fix the bug script can't print color on screen when system is windows

--- infolog.py
+++ infolog.py
@@ -4,8 +4,12 @@
 logging module for pybackup
 """
 import time
-import os, sys
+import os
+import sys
+import platform
+import ctypes
 import logging
+
 
 # console logger
 logger_con = logging.getLogger('pybackup')
@@ -32,7 +36,7 @@ datefmt = '%Y-%m-%d')
 logFilehandle.setFormatter(formatter_file)
 logger_file.addHandler(logFilehandle)
 
-def streamcol(s, col):
+def streamcol_unix(s, col, infotype):
     """
     change character's color when print on the screen
     """
@@ -49,7 +53,38 @@ def streamcol(s, col):
         out = yellow + s + end
     elif col == 'blue':
         out = blue + s + end
-    return out
+    text_console = "logger_con." + infotype + "(out)"
+    eval(text_console)
+
+def streamcol_win(s, col, infotype):
+    std_output_handle = -11
+    black = 0x00
+    white = 0x0f
+    green = 0x0a
+    red = 0x0c
+    yellow = 0x0e
+    blue = 0x0b
+    screenHandle = ctypes.windll.kernel32.GetStdHandle(std_output_handle)
+    if col == 'black':
+        ctypes.windll.kernel32.SetConsoleTextAttribute(screenHandle, black)
+    elif col == 'green':
+        ctypes.windll.kernel32.SetConsoleTextAttribute(screenHandle, green)
+    elif col == 'red':
+        ctypes.windll.kernel32.SetConsoleTextAttribute(screenHandle, red)
+    elif col == 'yellow':
+        ctypes.windll.kernel32.SetConsoleTextAttribute(screenHandle, yellow)
+    elif col == 'blue':
+        ctypes.windll.kernel32.SetConsoleTextAttribute(screenHandle, blue)
+    text_console = "logger_con." + infotype + "(s)"
+    eval(text_console)
+    ctypes.windll.kernel32.SetConsoleTextAttribute(screenHandle, 0x07)
+
+def streamcol(s, col, infotype):
+    if platform.system() == "Windows":
+        streamcol_win(s, col, infotype)
+    else:
+        streamcol_unix(s, col, infotype)
+
 
 def WriteLogFile(logname=None, filelist=None, chgfile=None, delfile=None, fullbackup=True):
     if fullbackup:
@@ -59,7 +94,7 @@ def WriteLogFile(logname=None, filelist=None, chgfile=None, delfile=None, fullba
         else:
             logname = logname+time.strftime('%Y-%m-%d-%H%M%S', time.localtime())+ '_FULL.log'
         logpath = os.path.join(os.path.abspath('..'), logname)
-        logger_con.info((streamcol('writing log file in path:{}'.format(logpath), 'blue')))
+        streamcol('writing log file in path:{}'.format(logpath), 'blue', 'info')
         logContent = ["file:{}    path:{}\n".format(key[0], key[1]) for key in filelist.keys()]
         endtext = "These files above were new files\n".format(time.strftime('%Y-%m-%d-%H%M%S', time.localtime()))
         logContent.append(endtext)
@@ -71,7 +106,7 @@ def WriteLogFile(logname=None, filelist=None, chgfile=None, delfile=None, fullba
         else:
             logname = logname + time.strftime('%Y-%m-%d-%H%M%S', time.localtime()) + '_INCR.log'
         logpath = os.path.join(os.path.abspath('..'), logname)
-        logger_con.info((streamcol('writing log file in path:{}'.format(logpath), 'blue')))
+        streamcol('writing log file in path:{}'.format(logpath), 'blue', 'info')
         chgContent = ["file:{}    path:{}\n".format(p[1], os.path.join(p[0], p[1]))
         for p in chgfile]
         chgContent.append('These files above have been modified or added\n\n\n')
@@ -79,3 +114,9 @@ def WriteLogFile(logname=None, filelist=None, chgfile=None, delfile=None, fullba
         delContent.append('These files above have been deleted, moved or renamed\n')
         with open(logpath, 'a', encoding='utf-8') as f:
             f.writelines(chgContent+delContent)
+
+
+
+if __name__ == '__main__':
+    testtxt = "haha"
+    streamcol(testtxt, 'green', 'info')
